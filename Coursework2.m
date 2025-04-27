d27e06bffa5360d93de8545360111d77fa700764
aryanbajaj13 <egyab26@nottingham.ac.uk>
2025-04-27 18:20:20 +0100

    Completed Prelim Task; LED Blink

--- Coursework2.m
+++ Coursework2.m
@@ -1,1 +1,42 @@
-%Coursework 2 
+% Aryan Bajaj   
+% egyab26@nottingham.ac.uk
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
+
+clear a
+clc
+a = arduino ('/dev/tty.usbmodem1201','Uno');
+writeDigitalPin(a,"D8",0);
+for i = 1:10
+    writeDigitalPin(a,"D8",1); %Turn LED ON
+    pause(0.5);                %Wait 0.5seconds
+    writeDigitalPin(a,"D8",0); %Turn LED OFF 
+    pause(0.5);                %Wait 0.5seconds
+end
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% Insert reflective statement here (400 words max)
+
+
+%% TASK 5 - COMMENTING, VERSION CONTROL AND PROFESSIONAL PRACTICE [15 MARKS]
+
+% No need to enter any answershere, but remember to:
+% - Comment the code throughout.
+% - Commit the changes to your git repository as you progress in your programming tasks.
+% - Hand the Arduino project kit back to the lecturer with all parts and in working order.
